9b37a9dceb6587038a3c7608fd341338fd79c9c5
Lucas Batista de Miranda <lucas.batista20012007@gmail.com>
2023-06-13 11:37:54 -0400

    first commit

---
+++ EMPREGADO.cs
@@ -0,0 +1,64 @@
+﻿class Empregado
+{
+    private string nomeEmpregado;
+    private double salarioEmpregado;
+    private DateTime dataAdmissaoEmpregado;
+
+    private static double somaSalarios = 0;
+
+    //===============================================[MÉTODO CONSTRUTOR]=============================================================
+    public Empregado(string nomeEmpregado, double salarioEmpregado, DateTime dataAdmissaoEmpregado)
+    {
+        this.nomeEmpregado = nomeEmpregado;
+        this.salarioEmpregado = salarioEmpregado;
+        this.dataAdmissaoEmpregado = dataAdmissaoEmpregado;
+
+        somaSalarios += salarioEmpregado;
+    }
+
+    //========================================================[GETS]=================================================================
+    public string GetNomeEmpregado()
+    {
+        return nomeEmpregado;
+    }
+    public double GetSalarioEmpregado()
+    {
+        return salarioEmpregado;
+    }
+
+    public static double GetSomaSalarios()
+    {
+        return somaSalarios;
+    }
+    public DateTime GetDataAdmissaoEmpregado()
+    {
+        return dataAdmissaoEmpregado;
+    }
+
+    //=======================================================[SETS]=====================================================================
+    public void SetSalario(double salario)
+    {
+        somaSalarios -= this.salarioEmpregado;
+        this.salarioEmpregado = salario;
+        somaSalarios += salario;
+    }
+
+    public void SetDataAdmissaoEmpregado(DateTime dataAdmissao)
+    {
+        this.dataAdmissaoEmpregado = dataAdmissao;
+    }
+    public void SetNomeEmpregado(string nome)
+    {
+        this.nomeEmpregado = nome;
+    }
+
+
+    //=======================================================[MÉTODO]=====================================================================
+
+    public void AumentarSalarioEmpregado(double percAumento)
+    {
+        double aumento = salarioEmpregado * (percAumento / 100);
+        salarioEmpregado += aumento;
+        somaSalarios += aumento;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+static void Main(string[] args)
+{
+
+    //========================================================[E1]============================================================================================
+    Empregado e1 = new Empregado("LUCAS ", 7600.0, new DateTime(2023, 1, 1));
+    Console.WriteLine($"Nome do empregado: {e1.GetNomeEmpregado()}, Salário do empregado: {e1.GetSalarioEmpregado()}, " +
+        $"Data de admissão do empregado: {e1.GetDataAdmissaoEmpregado()}");
+
+    Console.WriteLine($"Soma dos salários:  {Empregado.GetSomaSalarios()}");
+     e1.AumentarSalarioEmpregado(13);
+
+    Console.WriteLine($"Novo salário do empregado: {e1.GetNomeEmpregado()} {e1.GetSalarioEmpregado()}");
+    Console.WriteLine($"Soma dos salários após aumento: {0} ", Empregado.GetSomaSalarios());
+
+
+
+
+
+    //===========================================================[E2]=====================================================================================================]
+
+    Empregado e2 = new Empregado("LUCILEIDE", 9000.0, new DateTime(2012, 1, 1));
+    Console.WriteLine($"Nome do empregado: {e2.GetNomeEmpregado()}, Salário do empregado: {e2.GetSalarioEmpregado()}," +
+        $" Data de admissão do empregado: {e2.GetDataAdmissaoEmpregado()}");
+
+    Console.WriteLine($"Soma dos salários: {Empregado.GetSomaSalarios()}");
+
+    e2.AumentarSalarioEmpregado(9);
+    Console.WriteLine($"Novo salário da {e2.GetNomeEmpregado()}: {e2.GetSalarioEmpregado()}");
+    Console.WriteLine($"Soma dos salários após aumento: {Empregado.GetSomaSalarios()}");
+}
